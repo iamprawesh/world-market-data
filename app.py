46d9186009876401f8567278e03467a92160b275
Prawesh Panthi <PraweshPanthi@gmail.com>
2021-07-26 07:15:48 +0545

    added cors

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask,jsonify,render_template
 import requests
 from bs4 import BeautifulSoup
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
+
 
 @app.route("/")
 def index():
