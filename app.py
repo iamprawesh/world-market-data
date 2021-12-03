8f1954c0fb41aa52cc6761861ebe09b2fa5e39ce
Prawesh Panthi <PraweshPanthi@gmail.com>
2021-12-03 14:40:03 +0545

    order and txt file safe done

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def nepseData():
     point_change = nepse_div.find_all("div",class_=["point-change"])[0]
 
     data =  {
-            "name":"NEPSE",
+                    "name":"NEPSE",
             "point":point_div.text.strip(),
             "change":point_change.text.strip(),
             "order":1
@@ -71,7 +71,7 @@ def worldMarket():
                     "name":"NASDAQ",
                     "point":market_point,
                     "change":market_change,
-                    "order":2
+                    "order":10
 
                 }
              )  
@@ -81,7 +81,7 @@ def worldMarket():
                     "name":"DJI",
                     "point":market_point,
                     "change":market_change,
-                    "order":3
+                    "order":6
                 }
              )
         if 'ftse 100' in market_name.lower():
@@ -90,7 +90,7 @@ def worldMarket():
                     "name":"FTSE 100",
                     "point":market_point,
                     "change":market_change,
-                    "order":4
+                    "order":7
                 }
              )  
         if 'dax' in market_name.lower():
@@ -99,7 +99,7 @@ def worldMarket():
                     "name":"DAX",
                     "point":market_point,
                     "change":market_change,
-                    "order":5
+                    "order":8
                 }
              )  
         if 'cac' in market_name.lower():
@@ -108,7 +108,7 @@ def worldMarket():
                     "name":"CAC 40",
                     "point":market_point,
                     "change":market_change,
-                    "order":6
+                    "order":9
                 }
              )  
         if 'nikkei' in market_name.lower():
@@ -117,17 +117,17 @@ def worldMarket():
                     "name":"NIKKEI 225",
                     "point":market_point,
                     "change":market_change,
-                    "order":7
+                    "order":4
                 }
              )  
 
         if 'hang seng' in market_name.lower():
             x.append(
             {
-                    "name":" HANG SENG",
+                    "name":"HANG SENG",
                     "point":market_point,
                     "change":market_change,
-                    "order":8
+                    "order":5
                 }
              )   
         if 'sensex' in market_name.lower():
@@ -136,7 +136,7 @@ def worldMarket():
                     "name":"SENSEX",
                     "point":market_point,
                     "change":market_change,
-                    "order":9
+                    "order":2
                 }
              )  
 
@@ -146,7 +146,7 @@ def worldMarket():
                     "name":"NIFTY FIFTY",
                     "point":market_point,
                     "change":market_change,
-                    "order":10
+                    "order":3
                 }
              )  
     x = sorted(x ,key=lambda item:item['order'])
@@ -161,42 +161,53 @@ def index():
 					filemode='a',
                     level=logging.DEBUG
                     ) 
-    # logger=logging.getLogger() 
 
+    from os import path
+    ifexist = path.exists("data.text")
+    print(ifexist)
+    print("ifexist")
     try:
-        from os import path
-        ifexist = path.exists("data.txt")
         if ifexist:
-            file1 = open("data.txt","r+")
-            print("exist")
+            file1 = open("data.text","r+")
+            # print("exist")
             data = json.load(file1)
-            print(data)
+            # print(data)
             current = datetime.now()
             expires_time = data[0]['expires_in']
             # expires_time = str(datetime.now() -  timedelta(hours=2))
             #  
             expires = datetime.strptime(expires_time, '%Y-%m-%d %H:%M:%S.%f')
             current = datetime.strptime( str(datetime.now()), '%Y-%m-%d %H:%M:%S.%f')
+            print(expires)
+            print(current)
+            
             # print(data[1])
             if current > expires:
                 nepse = nepseData()
                 marketList.append(nepse)
+                fileinwite = open("data.text","w")
                 world = worldMarket()
-                marketList += world 
-                storeData(file1,marketList)
+                print("expired")
+                print("refetching")
+                marketList += world
+
+                storeData(fileinwite,marketList)
             else:
-                # load from json file
+                print("not expired")
                 marketList = data[1]
 
         else:
-            file1 = open("data.txt","w+")
+            print("file not exist")
+            fileinwite = open("data.text","w")
             nepse = nepseData()
             marketList.append(nepse)
             world = worldMarket()
             marketList += world 
-            storeData(file1,marketList)
+            storeData(fileinwite,marketList)
             print("not exist")
+        print("=============================")
         return jsonify(marketList)
+
 
 
     except Exception as e:
@@ -205,11 +216,9 @@ def index():
         # logger.WriteLines(str(datetime.now()) + ":" + str(log) +" "+e +"\n" )
         # logger.write("\n")
         marketList = []
+        print(e)
         # logging.error(traceback.format_exc())
-        # return jsonify([])
-    finally:
-        
-        file1.close()
+        return jsonify([])
         # logger.close()
         # return jsonify(marketList)
   
@@ -239,139 +248,149 @@ def index():
 
 def storeData(file1,data):
     expires_in = datetime.now() +  timedelta(hours=1)
-    # expires_in = plus3.strftime("%H:%M:%S")
     file1.write(json.dumps([{"expires_in":str(expires_in)},data]))
     file1.close()
 
 
 @app.route("/new")
-def new():
-    x = []
+def newv2():
+    marketList = []
+    nepse = nepseData()
+    marketList.append(nepse)
+    world = worldMarket()
+    marketList += world
+    return jsonify(marketList)
 
-    # for nepal
-    URL = "http://www.nepalstock.com/"
-    page = requests.get(URL)
 
-    soup = BeautifulSoup(page.content, "html.parser")
-    nepse_div = soup.find_all("div",class_=["pull-left", "banner-green", "banner-item"])[0]
-    point_div = nepse_div.find_all("div",class_=["current-index"])[0]
-    point_change = nepse_div.find_all("div",class_=["point-change"])[0]
+
+# @app.route("/new")
+# def new():
+#     x = []
+
+#     # for nepal
+#     URL = "http://www.nepalstock.com/"
+#     page = requests.get(URL)
+
+#     soup = BeautifulSoup(page.content, "html.parser")
+#     nepse_div = soup.find_all("div",class_=["pull-left", "banner-green", "banner-item"])[0]
+#     point_div = nepse_div.find_all("div",class_=["current-index"])[0]
+#     point_change = nepse_div.find_all("div",class_=["point-change"])[0]
  
-    x.append({
-            "name":"NEPSE",
-            "point":point_div.text.strip(),
-            "change":point_change.text.strip(),
-            "order":1
-            })
-    # for world
-    URL = "https://www.investing.com/indices/major-indices"
-    page = requests.get(URL)
+#     x.append({
+#             "name":"NEPSE",
+#             "point":point_div.text.strip(),
+#             "change":point_change.text.strip(),
+#             "order":1
+#             })
+#     # for world
+#     URL = "https://www.investing.com/indices/major-indices"
+#     page = requests.get(URL)
 
-    soup = BeautifulSoup(page.content, "html.parser")
-    # nifty_sensex = soup.find_all("div",class_=["txtmacmb","slick-slide", "slick-current", "slick-active"])
-    # print(nifty_sensex.ul)
-    y =  soup.find_all("tr", {"data-test":"price-row"})
-    # soup.find("div", {"class" : "data-sitekey"}))
-    # results = soup.find("div", class_="table-browser_table-browser-wrapper__2ynbE")
-    # print(y.tbody)
-    # print(len(y.tbody.tr))
-    for i in y:
-        # print(i.td[1].div.a)
-        all_td = i.find_all("td")
-        # name
-        market_name = all_td[1].div.a.text.strip()
-        market_point = all_td[2].text.strip(),
-        market_change = all_td[5].text.strip(),
+#     soup = BeautifulSoup(page.content, "html.parser")
+#     # nifty_sensex = soup.find_all("div",class_=["txtmacmb","slick-slide", "slick-current", "slick-active"])
+#     # print(nifty_sensex.ul)
+#     y =  soup.find_all("tr", {"data-test":"price-row"})
+#     # soup.find("div", {"class" : "data-sitekey"}))
+#     # results = soup.find("div", class_="table-browser_table-browser-wrapper__2ynbE")
+#     # print(y.tbody)
+#     # print(len(y.tbody.tr))
+#     for i in y:
+#         # print(i.td[1].div.a)
+#         all_td = i.find_all("td")
+#         # name
+#         market_name = all_td[1].div.a.text.strip()
+#         market_point = all_td[2].text.strip(),
+#         market_change = all_td[5].text.strip(),
 
-        market_change = ''.join(market_change)
-        market_point = ''.join(market_point)
-        if 'nasdaq' in market_name.lower():
-            # print(name)      
-            x.append(
-            {
-                    "name":"NASDAQ",
-                    "point":market_point,
-                    "change":market_change,
-                    "order":2
+#         market_change = ''.join(market_change)
+#         market_point = ''.join(market_point)
+#         if 'nasdaq' in market_name.lower():
+#             # print(name)      
+#             x.append(
+#             {
+#                     "name":"NASDAQ",
+#                     "point":market_point,
+#                     "change":market_change,
+#                     "order":2
 
-                }
-             )  
-        if 'dow' in market_name.lower():
-            x.append(
-            {
-                    "name":"DJI",
-                    "point":market_point,
-                    "change":market_change,
-                    "order":3
-                }
-             )
-        if 'ftse 100' in market_name.lower():
-            x.append(
-            {
-                    "name":"FTSE 100",
-                    "point":market_point,
-                    "change":market_change,
-                    "order":4
-                }
-             )  
-        if 'dax' in market_name.lower():
-            x.append(
-            {
-                    "name":"DAX",
-                    "point":market_point,
-                    "change":market_change,
-                    "order":5
-                }
-             )  
-        if 'cac' in market_name.lower():
-            x.append(
-            {
-                    "name":"CAC 40",
-                    "point":market_point,
-                    "change":market_change,
-                    "order":6
-                }
-             )  
-        if 'nikkei' in market_name.lower():
-            x.append(
-            {
-                    "name":"NIKKEI 225",
-                    "point":market_point,
-                    "change":market_change,
-                    "order":7
-                }
-             )  
+#                 }
+#              )  
+#         if 'dow' in market_name.lower():
+#             x.append(
+#             {
+#                     "name":"DJI",
+#                     "point":market_point,
+#                     "change":market_change,
+#                     "order":3
+#                 }
+#              )
+#         if 'ftse 100' in market_name.lower():
+#             x.append(
+#             {
+#                     "name":"FTSE 100",
+#                     "point":market_point,
+#                     "change":market_change,
+#                     "order":7
+#                 }
+#              )  
+#         if 'dax' in market_name.lower():
+#             x.append(
+#             {
+#                     "name":"DAX",
+#                     "point":market_point,
+#                     "change":market_change,
+#                     "order":8
+#                 }
+#              )  
+#         if 'cac' in market_name.lower():
+#             x.append(
+#             {
+#                     "name":"CAC 40",
+#                     "point":market_point,
+#                     "change":market_change,
+#                     "order":6
+#                 }
+#              )  
+#         if 'nikkei' in market_name.lower():
+#             x.append(
+#             {
+#                     "name":"NIKKEI 225",
+#                     "point":market_point,
+#                     "change":market_change,
+#                     "order":7
+#                 }
+#              )  
 
-        if 'hang seng' in market_name.lower():
-            x.append(
-            {
-                    "name":" HANG SENG",
-                    "point":market_point,
-                    "change":market_change,
-                    "order":8
-                }
-             )   
-        if 'sensex' in market_name.lower():
-            x.append(
-            {
-                    "name":"SENSEX",
-                    "point":market_point,
-                    "change":market_change,
-                    "order":9
-                }
-             )  
+#         if 'hang seng' in market_name.lower():
+#             x.append(
+#             {
+#                     "name":" HANG SENG",
+#                     "point":market_point,
+#                     "change":market_change,
+#                     "order":8
+#                 }
+#              )   
+#         if 'sensex' in market_name.lower():
+#             x.append(
+#             {
+#                     "name":"SENSEX",
+#                     "point":market_point,
+#                     "change":market_change,
+#                     "order":9
+#                 }
+#              )  
 
-        if 'nifty' in market_name.lower():
-            x.append(
-            {
-                    "name":"NIFTY FIFTY",
-                    "point":market_point,
-                    "change":market_change,
-                    "order":10
-                }
-             )  
-    x = sorted(x ,key=lambda item:item['order'])
-    return jsonify(x)
+#         if 'nifty' in market_name.lower():
+#             x.append(
+#             {
+#                     "name":"NIFTY FIFTY",
+#                     "point":market_point,
+#                     "change":market_change,
+#                     "order":10
+#                 }
+#              )  
+#     x = sorted(x ,key=lambda item:item['order'])
+#     return jsonify(x)
 
 
 @app.route("/exp")
@@ -393,11 +412,16 @@ def Test():
 @app.route('/test', methods=["GET",'POST'])
 def my_form_post():
     # email = None
-
+    
     text = request.form['text']
     processed_text = text.upper()
     print(processed_text)
+
     return processed_text
+
+@app.route("/v2")
+def v2():
+    return "Hello World!"
 
 if __name__ == "__main__":
     from waitress import serve
